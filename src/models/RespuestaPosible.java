f312ee26439b349f2288494c1ab3119add1d51d5
Nicolás Agustín Enrico <91215@sistemas.frc.utn.edu.ar>
2023-06-08 09:14:42 -0300

    rtaPosible

--- src/models/RespuestaPosible.java
+++ src/models/RespuestaPosible.java
@@ -15,18 +15,7 @@ public class RespuestaPosible {
         this.valor = valor;
     }
     
-    //Metodo usado para la busqueda de respuestas, para esTuPregunta
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null || getClass() != obj.getClass()) {
-            return false;
-        }
-        RespuestaPosible otraRespuesta = (RespuestaPosible) obj;
-        return this.valor == otraRespuesta.valor && this.descripcion.equals(otraRespuesta.descripcion);
-    }
+
     
     //Metodos de seteo
     public String getDescripcion() {
